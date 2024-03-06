a5d1df351ed5238eb212ae71b12151270f8edd90
MoonKiHyun <moonkihyun95@gmail.com>
2024-03-06 19:16:12 +0900

    git init

---
+++ src/main/java/com/moon/questgraph/QuestGraphApplication.java
@@ -0,0 +1,13 @@
+package com.moon.questgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestGraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestGraphApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moon/questgraph/QuestGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moon.questgraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestGraphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
